1e073f1bc1b7d1ccab3582afb2d76380ef6d3715
Daniil <bondarev@surf.dev>
2024-03-14 16:38:55 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,56 @@
-void main() {
-  while (!wantSleep()) {
-    if (wantEat()) {
-      eat();
-    } else {
-      if (needMoney() case [amount(), 1000000]) {
-        needALotOfMoney();
-        workHardly();
-      } else if (needMoney() case [amount(), 1000]) {
-        needSomeMoney();
-        work();
-      } else {
-        needNoMoney();
-        for (int i = 0; i < 11; i++) {
-          sleep();
-          i == 7 ? turnOffAlarm() : snooze();
-          if (i == 9) {
-            wakeUp();
-            break;
-          }
-        }
-      }
-    }
-  }
+// ignore_for_file: prefer_final_fields, unused_field, prefer_function_declarations_over_variables, avoid_print
+
+typedef Strategy = double Function(List<String>, List<String>);
+
+class PokerPlayer {
+  /// Список текущих карт в руке у игрока
+  List<String> _currentHand = ['King of clubs', 'Nine of hearts'];
+
+  /// Субъективная оценка выигрыша
+  double surenessInWin = 0;
+
+  /// Вычислить шансы на выигрыш
+  void calculateProbabilities(
+    List<String> cardOnDesk,
+
+    /// Это часть первого задания. [Strategy] пока не сущестивует.
+    ///
+    /// Опишите его.
+    Strategy strategy,
+  ) =>
+      surenessInWin = strategy(cardOnDesk, _currentHand);
 }
 
-bool wantEat() {
-  switch (wantFood()) {
-    case 'meat':
-      wantMeat();
-      return true;
-    case 'nuts':
-      wantNuts();
-      return true;
-    default:
-      wantNothing();
-      return false;
-  }
+void main() {
+  final opponent = PokerPlayer();
+
+  /// Это часть первого задания. [Strategy] пока не сущестивует.
+  ///
+  /// Опишите его.
+  final Strategy fakeStrategy = (p0, p1) {
+    print(
+      'Карты на столе: '
+      '${p0.reduce(
+        (value, element) => '$value, $element',
+      )}',
+    );
+    print(
+      'Карты в руке оппонента: '
+      '${p1.reduce(
+        (value, element) => '$value, $element',
+      )}',
+    );
+
+    return 0.31;
+  };
+
+  opponent.calculateProbabilities(
+    ['Nine of diamonds', 'King of hearts'],
+    fakeStrategy,
+  );
+
+  print(
+    'Шансы оппонента на победу: '
+    '${opponent.surenessInWin}',
+  );
 }
